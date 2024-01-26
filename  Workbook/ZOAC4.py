ebcad63dd86354abdd1bcdc08ec700cb638753fa
Minkon Kim <alsrhs98@gmail.com>
2024-01-26 09:47:13 +0900

    2024.01.26

---
+++  Workbook/ZOAC4.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open("../input.txt","r")
+
+H, W, N, M = map(int, input().split())
+
+col = H//(N+1) if H%(N+1) == 0 else H//(N+1)+1
+row = W//(N+1) if W%(M+1) == 0 else W//(M+1)+1
+                            print(col*row)
+
+
