286d5601cf4c9283f6e7442481ce1f944866d2a1
Minkon Kim <alsrhs98@gmail.com>
2024-03-14 12:43:48 +0900

    2024.03.13

--- Greedy/CEO_DongHyuk.py
+++ Greedy/CEO_DongHyuk.py
@@ -1,5 +1,5 @@
 import sys
-sys.stdin = open('C:\\CodeTest_py\\input.txt', "r")
+sys.stdin = open('../input.txt', "r")
 #  Qu 0.25 Di 0.10 Ni 0.05 Pe 0.01
 # =>   25       10       5       1
 def calCoins(Money):
@@ -7,16 +7,16 @@ def calCoins(Money):
    Di =0
    Ni =0 
    Pe =0
-   if (Money > 25):
+   if (Money >= 25):
        Qu = Money//25
        Money -= Qu*25
-   if (Money > 10):
+   if (Money >= 10):
        Di = Money//10
        Money -= Di*10
-   if (Money > 5):
+   if (Money >= 5):
        Ni = Money//5
        Money -= Ni*5
-   if (Money > 1):
+   if (Money >= 1):
        Pe = Money
 
    print(Qu, Di, Ni, Pe)
